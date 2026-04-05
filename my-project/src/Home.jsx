71417ac72e72fe4295156ae90887917306ce62cd
Shashank-518 <shashank5418shashu@gmail.com>
2026-04-05 19:59:58 +0530

    Prod changes

--- my-project/src/Home.jsx
+++ my-project/src/Home.jsx
@@ -469,7 +469,7 @@ export default function Home() {
     };
 
     try {
-      const res = await fetch("http://localhost:8000/api/search", {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/search`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
